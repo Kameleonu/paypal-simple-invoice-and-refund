3134d01c4844b21a7899bd4494d67b5f3b22c5fb
root <root@localhost.localdomain>
2019-12-29 13:40:06 +0200

    ref

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
-$clientId = "AUqy2YNgVLmVUqJmkWp-h2baX3qiF5BqcM4oT3AVAH-rNxgKlZbqk_wzAAKozImR-4syvY6m_Kf7yOeg";
-$secret = "ED3_SSwjDBpPqcN7JBfAHH7YXU_XbElpRC5I9diCQ9wJvoYpNwCJDTRJOIa_tv4_QvTKEt7U6I2_PZB0";
-$sandbox = true;
-$payPalEmail = 'sb-ooqpl805313@business.example.com';
-$givenName = 'John';
-$surName = 'Doe';
-$note = 'Thank you for your business.';
-$term = 'No refunds after 30 days.';
-$currencyCode = 'USD';
-$additionalNotes = 'Additional Notes Here';
+$clientId           = "**CLIENT-ID**";
+$secret             = "**SECRET**";
+$sandbox            = true;
+$payPalEmail        = 'sb-ooqpl805313@business.example.com';
+$givenName          = 'John';
+$surName            = 'Doe';
+$note               = 'Thank you for your business.';
+$term               = 'No refunds after 30 days.';
+$currencyCode       = 'USD';
+$additionalNotes    = 'Additional Notes Here';
 ?>
